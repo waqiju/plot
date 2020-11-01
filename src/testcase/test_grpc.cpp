98a7d393993f044f3e3ca8d9d5f1daaf2eb42e79
waqiju <waqiju@gmail.com>
2020-11-02 00:20:51 +0800

    Modify: disable warning 4251

--- src/testcase/test_grpc.cpp
+++ src/testcase/test_grpc.cpp
@@ -2,6 +2,7 @@
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
+#pragma warning(disable: 4251)
 #include "pb/chimera.grpc.pb.h"
 
 using grpc::Server;
